b01124435f286996d47b7839e385afff61116fda
Ivo <ivocardozo7@gmail.com>
2020-05-03 11:50:29 -0400

    Basic WebService was implemented

--- src/main/java/com/jalasoft/practice/Main.java
+++ src/main/java/com/jalasoft/practice/Main.java
@@ -1,4 +1,12 @@
 package com.jalasoft.practice;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
 public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello!!**************************");
+        SpringApplication.run(Main.class, args);
+    }
 }
